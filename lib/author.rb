66ea57897eeba9d698549de9e04cee86caccd799
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-14 02:31:27 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,8 +15,8 @@ class Author
   end
 
   def add_post_by_title(title)
-    song = Song.new(name)
-    song.artist = self
+    song = Song.new(title)
+    song.author = self
   end
 
   def post_count
