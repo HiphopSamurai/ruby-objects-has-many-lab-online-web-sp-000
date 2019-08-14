c45a05fe4a9a35c34eda3ed0872ac041ba4f67ba
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-14 02:37:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,9 +19,10 @@ class Artist
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
+    @@song_count += 1    
   end
 
   def self.song_count
-    ####
+    @@song_count
   end
 end
